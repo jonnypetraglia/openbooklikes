857be37f9a8874bcbf7c0197353b93a0eb3e3eae
Jon Petraglia <notbryant@gmail.com>
2016-02-13 02:09:35 -0700

    Thing

--- readingchallengeparser/src/main/java/com/qweex/openbooklikes/challenge/ReadingChallengeParser.java
+++ readingchallengeparser/src/main/java/com/qweex/openbooklikes/challenge/ReadingChallengeParser.java
@@ -7,10 +7,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.net.SocketTimeoutException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.concurrent.TimeoutException;
 
 /*
 java -cp .:/Users/notbryant/.gradle/caches/modules-2/files-2.1/org.jsoup/jsoup/1.8.3/65fd012581ded67bc20945d85c32b4598c3a9cf1/jsoup-1.8.3.jar ReadingChallengeParser
@@ -20,35 +18,21 @@ java -cp .:/Users/notbryant/.gradle/caches/modules-2/files-2.1/org.jsoup/jsoup/1
 public class ReadingChallengeParser {
     Document doc;
     public int total, current;
-    public String year;
+    public String year, userId;
     public String[] past;
     public double percentageOfYear;
     double percentageOfBooks;
 
-    // Here's the widget way
-//    URL = "http://booklikes.com/widget/readingchallenge?id=%1s&year=%2s",
-//    current = Integer.parseInt(doc.select(".info > span:nth-of-type(1)").first().text());
-//    total = Integer.parseInt(doc.select(".info > span:nth-of-type(2)").first().text());
-
-
     private static final String
             URL = "http://booklikes.com/apps/reading-challenge/%1s/%2s";
 
-    public ReadingChallengeParser(int total, int current, String year, String[] past)  {
-        this.year = year;
-        this.total = total;
-        this.current = current;
-        this.past = past;
-    }
-
-    public ReadingChallengeParser(String userId, String year) throws IOException, NoSuchChallengeException {
-        String fullUrl = String.format(URL, userId, year);
+    public ReadingChallengeParser(String userId, String yearToLoad) throws IOException, NoSuchChallengeException {
+        this.userId = userId;
+        this.year = yearToLoad;
         doc = Jsoup
-                .connect(fullUrl)
+                .connect(url())
                 .timeout(6000)
                 .get();
-
-        System.out.println("WOOOOOOHOOOOOO");
 
         // Attempt to snag the past years
         try {
@@ -56,9 +40,13 @@ public class ReadingChallengeParser {
 
             this.past = new String[pastEls.size() - 1];
             for(int i=0, j=0; i<pastEls.size(); i++) {
-                if(pastEls.get(i).hasClass("nav-active"))
-                    this.year = pastEls.get(i).text();
-                else
+                Element element = pastEls.get(i);
+                if(element.hasClass("nav-active")) {
+                    if(this.year.length()==0)
+                        this.year = element.text();
+                    else if(!this.year.equals(element.text()))
+                        throw new RuntimeException("This should be impossible: the year sought was not found (" + this.year + " vs " + element.text() + ")");
+                }else
                     this.past[j++] = pastEls.get(i).text();
             }
 
@@ -75,22 +63,12 @@ public class ReadingChallengeParser {
         percentageOfBooks = 100.0*current / total;
     }
 
-    public static class NoSuchChallengeException extends RuntimeException {
-        public NoSuchChallengeException(String message) {
-            super(message);
-        }
-    }
-
     public ReadingChallengeParser(String userId) throws IOException {
         this(userId, Integer.toString(Calendar.getInstance().get(Calendar.YEAR)));
     }
 
-    public ReadingChallengeParser(int userId) throws IOException {
-        this(Integer.toString(userId));
-    }
-
-    public ReadingChallengeParser(int userId, String year) throws IOException {
-        this(Integer.toString(userId), year);
+    public String url() {
+        return String.format(URL, userId, year);
     }
 
     public double daysRemaining() {
@@ -112,6 +90,12 @@ public class ReadingChallengeParser {
 
     public double behind() {
         return (percentageOfYear-percentageOfBooks)/100 * total;
+    }
+
+    public static class NoSuchChallengeException extends RuntimeException {
+        public NoSuchChallengeException(String message) {
+            super(message);
+        }
     }
 
     public static void main(String[] args) {
